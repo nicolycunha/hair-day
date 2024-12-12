5acf60615f8cc456eadb3151fc82b3be5afa18f2
Nicoly Cunha <nicolyoliveiracunha.2001@gmail.com>
2024-12-12 14:31:49 -0300

    change server.js to load api

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,9 +1,19 @@
-const fs = require('fs');
+const fs = require('fs')
+const path = require('path')
 
-exports.handler = async function(event, context) {
-  const data = JSON.parse(fs.readFileSync('server.json', 'utf8'));
-  return {
-    statusCode: 200,
-    body: JSON.stringify(data),
-  };
-};
+exports.handler = async function (event, context) {
+    const filePath = path.resolve(__dirname, '../../server.json')
+
+    try {
+        const data = JSON.parse(fs.readFileSync(filePath, 'utf8'))
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data)
+        }
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Erro ao ler o arquivo server.json' })
+        }
+    }
+}
