1e3ed202f9cbc36ffc124030a484b090a0c8f542
Nicoly Cunha <nicolyoliveiracunha.2001@gmail.com>
2024-12-12 11:51:29 -0300

    feat: cancel schedule and adjustments on form

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,5 +1,6 @@
 import dayjs from 'dayjs'
 import { scheduleNew } from '../../services/schedule-new'
+import { schedulesDay } from '../schedules/load.js'
 
 const form = document.querySelector('form')
 const clientName = document.getElementById('client')
@@ -29,13 +30,17 @@ form.onsubmit = async event => {
         const [hour] = hourSelected.innerText.split(':')
         const when = dayjs(selectedDate.value).add(hour, 'hour')
 
-        const id = new Date().getTime()
+        const id = new Date().getTime().toString()
 
         await scheduleNew({
             id,
             name,
             when
         })
+
+        await schedulesDay()
+
+        clientName.value = ''
     } catch (error) {
         alert('Não foi possível realizar o agendamento')
         console.log(error)
